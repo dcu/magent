a29fbe3cb36779f694a9bcacbb0287bdc8d2192b
David A. Cuadrado <krawek@gmail.com>
2009-10-14 00:45:14 -0500

    method to close safely (hopefully)
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- lib/magent/processor.rb
+++ lib/magent/processor.rb
@@ -4,6 +4,7 @@ module Magent
 
     def initialize(actor)
       @actor = actor
+      @shutdown = false
 
       @actor.class.actions.each do |action|
         if !@actor.respond_to?(action)
@@ -15,36 +16,52 @@ module Magent
     def run!
       processed_messages = 0
       delay = 0
+
+      trap('TERM') { shutdown!; exit 0 }
+      trap('SIGINT') { shutdown!; exit 0 }
+
       loop do
+        break if @shutdown
+
         delay = 0 if @actor._run_tasks
 
-        method, payload = @actor.class.channel.dequeue
+        @method, @payload = @actor.class.channel.dequeue
 
-        if method.nil?
+        if @method.nil?
           delay += 0.1 if delay <= 5
         else
           delay = 0
-          $stderr.puts "#{@actor.class}##{method}(#{payload.inspect})"
+          $stderr.puts "#{@actor.class}##{@method}(#{@payload.inspect})"
           begin
-            if @actor.class.actions.include?(method)
+            if @actor.class.actions.include?(@method)
               processed_messages += 1
-              @actor.send(method, payload)
+              @actor.send(@method, @payload)
 
               if processed_messages > 20
                 processed_messages = 0
                 GC.start
               end
             else
-              $stderr.puts "Unknown action: #{method} (payload=#{payload.inspect})"
+              $stderr.puts "Unknown action: #{@method} (payload=#{@payload.inspect})"
             end
+          rescue SystemExit
           rescue Exception => e
-            $stderr.puts "Error while executing #{method.inspect} #{payload.inspect}"
-            $stderr.puts "#{e.to_s} #{e.backtrace.join("\t\n")}"
-            @actor.class.channel.failed(:message => e.message, :method => method, :payload => payload, :backtrace => e.backtrace)
+            $stderr.puts "Error while executing #{@method.inspect} #{@payload.inspect}"
+            $stderr.puts "#{e.to_s}\n#{e.backtrace.join("\t\n")}"
+            @actor.class.channel.failed(:message => e.message, :method => @method, :payload => @payload, :backtrace => e.backtrace)
+          ensure
+            @method, @payload = nil
           end
         end
-
         sleep delay
+      end
+    end
+
+    def shutdown!
+      @shutdown = true
+      $stderr.puts "Shutting down..."
+      if @method
+        @actor.class.channel.enqueue(@method, @payload)
       end
     end
   end #Processor
