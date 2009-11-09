ee7343def48e4be0a30ec9ea897e291388c5cc85
David A. Cuadrado <krawek@gmail.com>
2009-11-08 19:12:07 -0500

    methods to retry and remove errors
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- lib/magent/channel.rb
+++ lib/magent/channel.rb
@@ -15,6 +15,16 @@ module Magent
       error_collection.find({:channel_id => @name}, {:offset => (page-1)*per_page, :limit => per_page, :sort => ["created_at"]})
     end
 
+    def remove_error(error_id)
+      object_id = Mongo::ObjectID.from_string(error_id)
+      self.error_collection.remove(:_id => object_id, :channel_id => @name)
+    end
+
+    def retry_error(error)
+      remove_error(error["_id"])
+      enqueue(error["method"], error["payload"])
+    end
+
     def error_collection
       @error_collection ||= Magent.database.collection("errors")
     end
